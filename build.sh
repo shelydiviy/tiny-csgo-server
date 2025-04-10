74ce9db50a9fb33ee46810683ec46d6f1907597b
Shkrest <telephon.inet@gmail.com>
2025-04-10 12:12:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HL2SDK_DIR=""
-ASIO_DIR=""
+HL2SDK_DIR="/home/sprut/mirror/hl2sdk"
+ASIO_DIR="/home/sprut/mirror/asio/asio"
 
 cmake -DCMAKE_BUILD_TYPE=Release -DHL2SDK-CSGO:PATH=$HL2SDK_DIR -DASIO-SRC:PATH=$ASIO_DIR -DEXECUTABLE_OUTPUT_PATH=$PWD/build/Release
 make
